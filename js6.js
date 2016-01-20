0735e9f758a7c29a742fa80a24ca56e0126b7d20
Student <student@RU-003.local>
2016-01-20 14:31:14 -0700

    completed exercise

---
+++ js6.js
@@ -0,0 +1,82 @@
+var app = angular.module("app", []);
+
+
+angular.module('app')
+	.controller('profileController', ['$scope', function($scope){
+
+		$scope.userName = "John Wayne"
+
+		$scope.form = {
+			show: false,
+			content: ["Show Form", "Hide Form"],
+		}
+
+		$scope.profilePic = {
+			url: './profile-picture.png',
+			show: true
+		}
+
+		$scope.formValues = {
+			userName : $scope.userName,
+		}
+
+		$scope.blankFormValues = function() {
+			$scope.formValues = {
+				userName : '',
+				profilePic : {},
+				paragraphs : {},
+				lists : []
+			}
+		}
+
+		$scope.setFormValues = function() {
+			$scope.formValues = {
+				userName : $scope.userName,
+				profilePic : Object.assign({}, $scope.profilePic),
+				paragraphs : Object.assign({}, $scope.paragraphs),
+				lists : angular.copy($scope.lists),
+			}
+		}
+
+		$scope.saveFormValues = function() {
+			$scope.userName = $scope.formValues.userName;
+			$scope.profilePic = $scope.formValues.profilePic;
+			$scope.paragraphs = Object.assign({}, $scope.formValues.paragraphs);
+			$scope.lists = angular.copy($scope.formValues.lists);
+		}
+
+		$scope.paragraphs = {
+			name : 'About Me',
+			content : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+				show : true
+		}
+
+		$scope.lists = [
+			{
+				name: 'Favorite Books',
+				listContent: [
+					{value: 'Pirates', show: true},
+					{value: 'Robots', show: true},
+					{value: 'Zombies', show: true}
+				]
+			},
+			{
+				name: 'Favorite JS Libraries',
+				listContent: [
+					{value: 'The Bird with Crystal Plumage', show: true},
+					{value: 'School of the Holy Beast', show: true},
+					{value: 'Cemetary Man', show: true}
+				]
+			},
+			// {
+			// 	name: 'Languages Known',
+			// 	listContent: [
+			// 		{value: 'Sumerian', show: true},
+			// 		{value: 'Linear B', show: true},
+			// 		{value: 'Cockney', show: true}
+			// 	]
+			// },
+		]
+
+
+}])
